4e3a15656e099c4d831de65635f4c1bcbbad6665
FasoleWarior <45045665+FasoleWarior@users.noreply.github.com>
2021-03-23 18:58:34 +0200

    Added animations and reset

--- ChessMain.py
+++ ChessMain.py
@@ -29,6 +29,7 @@ def main():
     running = True
     sqSelected = ()
     positions = []
+    animate = False
     while running:
         for e in p.event.get():
             if e.type == p.QUIT:
@@ -49,15 +50,26 @@ def main():
                         if move == validMoves[i]:
                             state.makeMove(move)
                             moveMade = True
+                            animate = True
                             sqSelected = ()
                             positions = []
                     if not moveMade:
                         positions = []
             elif e.type == p.KEYDOWN:
-                if e.key == p.K_r:
+                if e.key == p.K_u: # undo move when key "U" is pressed
                     state.undoMove()
                     moveMade = True
+                    animate = False
+                if e.key == p.K_r: # reset game when key "R" is pressed
+                    state = GameState()
+                    validMoves = state.getValidMoves()
+                    sqSelected = ()
+                    positions = []
+                    animate = False
+                    moveMade = False
         if moveMade:
+            if animate:
+                moveAnimation(state.moveLog[-1], screen, state.board, clock)
             validMoves = state.getValidMoves()
             moveMade = False
         drawGameState(screen, state, positions, validMoves)
@@ -66,16 +78,25 @@ def main():
 
 def drawGameState(screen, state, positions, moves):
     drawBoard(screen)
-    drawPieces(screen, state.board, positions, moves)
+    drawPieces(screen, state.board)
+    highlightSquares(screen, state.board, positions, moves)
 
 def drawBoard(screen):
+    global colors
     colors = [p.Color("gray"), p.Color("dark gray")]
     for r in range(DIMENSION):
         for c in range(DIMENSION):
             color = colors[(r+c) % 2]
             p.draw.rect(screen, color, p.Rect(c*SQUARE, r*SQUARE, SQUARE, SQUARE))
 
-def drawPieces(screen, board, positions, moves):
+def drawPieces(screen, board):
+    for r in range(DIMENSION):
+        for c in range(DIMENSION):
+            piece = board[r][c]
+            if piece != '--':
+                screen.blit(IMAGES[piece], p.Rect(c*SQUARE, r*SQUARE, SQUARE, SQUARE))
+
+def highlightSquares(screen, board, positions, moves):
     viable = []
     if len(positions) == 1:
         x = positions[0][0]
@@ -84,13 +105,29 @@ def drawPieces(screen, board, positions, moves):
         for move in moves:
             if (move.startR, move.startC) == (x, y):
                 viable.append((move.endR, move.endC))
-    for r in range(DIMENSION):
-        for c in range(DIMENSION):
-            piece = board[r][c]
-            if piece != '--':
-                screen.blit(IMAGES[piece], p.Rect(c*SQUARE, r*SQUARE, SQUARE, SQUARE))
-            if (r, c) in viable:
-                p.draw.circle(screen, p.Color("red"), (c * SQUARE + SQUARE // 2, r * SQUARE + SQUARE // 2), SQUARE // 6)
+        for (r, c) in viable:
+            p.draw.circle(screen, p.Color("red"), (c * SQUARE + SQUARE // 2, r * SQUARE + SQUARE // 2), SQUARE // 6)
+
+def moveAnimation(move, screen, board, clock):
+    global colors
+    coordinates = []
+    dR = move.endR - move.startR
+    dC = move.endC - move.startC
+    FPS = 10
+    frameCount = (abs(dR) + abs(dC))*FPS
+    for frame in range(frameCount + 1):
+        x, y = (move.startR + dR*frame/frameCount, move.startC + dC*frame/frameCount)
+        drawBoard(screen)
+        drawPieces(screen, board)
+        color = colors[(move.endR + move.endC) % 2]
+        endSquare = p.Rect(move.endC * SQUARE, move.endR*SQUARE, SQUARE, SQUARE)
+        p.draw.rect(screen, color, endSquare)
+        if move.pieceCaptured != '--':
+            screen.blit(IMAGES[move.pieceCaptured], endSquare)
+        screen.blit(IMAGES[move.pieceMoved], p.Rect(y*SQUARE, x*SQUARE, SQUARE, SQUARE))
+        p.display.flip()
+        clock.tick(60)
+
 
 def playingvsBot():
     p.init()
@@ -104,6 +141,7 @@ def playingvsBot():
     running = True
     sqSelected = ()
     positions = []
+    animate = False
     bot = RandomBot  # bot takes the type of AI you'll play against, for now only RandomBot is available
     while running:
         if state.whiteMoves:
@@ -128,17 +166,28 @@ def playingvsBot():
                                     moveMade = True
                                     sqSelected = ()
                                     positions = []
+                                    animate = True
                             if not moveMade:
                                 positions = []
                 elif e.type == p.KEYDOWN:
-                    if e.key == p.K_r:
+                    if e.key == p.K_u:  # undo move when key "U" is pressed
                         state.undoMove()
                         moveMade = True
+                        animate = False
+                    if e.key == p.K_r:  # reset game when key "R" is pressed
+                        state = GameState()
+                        validMoves = state.getValidMoves()
+                        sqSelected = ()
+                        positions = []
+                        animate = False
+                        moveMade = False
         else:
             index = bot(validMoves)
             state.makeMove(validMoves[index])
             moveMade = True
         if moveMade:
+            if animate:
+                moveAnimation(state.moveLog[-1], screen, state.board, clock)
             validMoves = state.getValidMoves()
             moveMade = False
         drawGameState(screen, state, positions, validMoves)
@@ -146,4 +195,4 @@ def playingvsBot():
         p.display.flip()
 
 
-playingvsBot()
+main()
